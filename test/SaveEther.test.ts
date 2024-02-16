bb40d24647e31da19f99a7d7f3923c992dd38c55
AbelOsaretin <contact.abel321@gmail.com>
2024-02-16 15:34:25 +0100

    Writing test

---
+++ test/SaveEther.test.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+// Right click on the script name and hit "Run" to execute
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("SaveERC20", function () {
+  it("test deposit into contract", async function () {
+    const SaveERC20 = await ethers.getContractFactory("SaveERC20");
+    const contract = await SaveERC20.deploy();
+    await contract.deployed();
+    console.log("storage deployed at:" + contract.address);
+    const _amount = 10;
+    //expect((await contract.deposit(_amount)));
+  });
+  //   it("test updating and retrieving updated value", async function () {
+  //     const Storage = await ethers.getContractFactory("Storage");
+  //     const storage = await Storage.deploy();
+  //     await storage.deployed();
+  //     const storage2 = await ethers.getContractAt("Storage", storage.address);
+  //     const setValue = await storage2.store(56);
+  //     await setValue.wait();
+  //     expect((await storage2.retrieve()).toNumber()).to.equal(56);
+  //   });
+});
